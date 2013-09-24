91540e3de48ca7b9e48b9239003d892ea42fd340
oops <oops@4e5e38c9-b7c8-4486-8525-9d64f9946180>
2013-09-24 15:54:15 +0000

    add test-relay.php

--- trunk/tests/test.php
+++ trunk/tests/test.php
@@ -3,6 +3,18 @@
  * Test code for pear_HTTPRelay
  * $Id$
  */
+
+$iniget = function_exists ('___ini_get') ? '___ini_get' : 'ini_get';
+$iniset = function_exists ('___ini_set') ? '___ini_set' : 'ini_set';
+
+$cwd = getcwd ();
+$ccwd = basename ($cwd);
+if ( $ccwd == 'tests' ) {
+	$oldpath = $iniget ('include_path');
+	$newpath = preg_replace ("!/{$ccwd}!", '', $cwd);
+	$iniset ('include_path', $newpath . ':' . $oldpath);
+}
+
 require_once 'HTTPRelay.php';
 
 $http = new HTTPRelay;
